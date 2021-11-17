bf74ef417f6a1382e15c4d2404b093298f1c28cf
alejandro <alejandro@softer.com.ar>
2021-11-17 10:38:12 -0300

    modifico menu

--- components/menuModulos.js
+++ components/menuModulos.js
@@ -1,11 +1,12 @@
 import { Icon, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import useSWR from 'swr';
 import Link from 'next/link';
+import Loader from './loader';
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function MenuModulos({}){
     const {data} = useSWR("/api/modulos/mods",fetcher);
-    
+    if(!data)return <Loader texto="Cargando menu"/>
     return(
         
         <List component="div" disablePadding>
