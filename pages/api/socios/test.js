38b4a7ab5dea4a6d73252812b084fbf80f577775
alejandro <alejandro@softer.com.ar>
2022-02-04 10:30:06 -0300

    add test socios

---
+++ pages/api/socios/test.js
@@ -0,0 +1,7 @@
+import {countCollection, findAll} from "../../../config/firebase";
+import handlerApiABM from "../../../helpers/handlerApiABM";
+export default async function handler(req, res) {
+    const { id } = req.query
+    
+    res.status(200).json({"test":"ok"})
+}
