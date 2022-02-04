0e6629cae8660d2b05e11181ac30b6ecb564f194
alejandro <alejandro@softer.com.ar>
2022-02-04 12:15:05 -0300

    test auth branch

--- pages/api/socios/test.js
+++ pages/api/socios/test.js
@@ -3,5 +3,5 @@ import handlerApiABM from "../../../helpers/handlerApiABM";
 export default async function handler(req, res) {
     const { id } = req.query
     
-    res.status(200).json({"test":"ok"})
+    res.status(200).json({"esto ":"esto es un testo de brach newAUth"})
 }
