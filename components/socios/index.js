200775559485cec852142306bb8f422c020c26f8
alejandro <alejandro@softer.com.ar>
2022-02-04 08:26:39 -0300

    test

--- components/socios/index.js
+++ components/socios/index.js
@@ -2,12 +2,13 @@
 import Stack from '@mui/material/Stack';
 import { Grid } from "@mui/material";
 import BuscadorSocios from "./buscadorSocios";
+import ErrorBoundary from "../../helpers/errorBoundary"
 export default function Modulo({modulo,mod,token}) {
 
       return (
       <Stack flex={1} spacing={2}>
             <Grid flex={1}>
-                  <BuscadorSocios modulo={modulo} mod={mod} token={token} />
+                 <ErrorBoundary> <BuscadorSocios modulo={modulo} mod={mod} token={token} /></ErrorBoundary>
             </Grid>
       </Stack>
       )
