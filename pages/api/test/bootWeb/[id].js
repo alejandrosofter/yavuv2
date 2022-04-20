41413a70561ea7c9ce76d10d4740954109024cbe
alejandro <alejandro@softer.com.ar>
2022-04-20 09:17:23 -0300

    test

--- pages/api/test/bootWeb/[id].js
+++ pages/api/test/bootWeb/[id].js
@@ -1,11 +1,16 @@
-import puppeteer from 'puppeteer'
+const chromium = require('chrome-aws-lambda');
+const playwright = require('playwright-core');
 
 import {findOne} from "../../../../config/firebase"
 const TIME_OUT_INNERTEXT=5000
 
 export default async function handler(req, res) {
 
-    const browser = await puppeteer.launch();
+    const browser = await playwright.chromium.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+      })
   const page = await browser.newPage();
   await page.goto('https://google.com');
   await page.screenshot({ path: 'example.png' });
