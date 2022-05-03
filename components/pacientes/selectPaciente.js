35b92942af325bab4699b1fa8e03dcab18f89a38
alejandro <alejandro@softer.com.ar>
2022-05-03 09:34:52 -0300

    turnos arreglos

--- components/pacientes/selectPaciente.js
+++ components/pacientes/selectPaciente.js
@@ -1,12 +1,31 @@
+import DialogContenido from '@components/forms/dialogContenido';
+import { getModUsuario } from '@helpers/db';
+import { Button, Grid, Icon, Tooltip } from '@mui/material';
 import { useCollection,fuego } from '@nandorojo/swr-firestore';
+import { useState } from 'react';
 import Select2 from "../forms/select2Formik"
+import NuevoPaciente from "./nuevo"
 export default function Modulo({label,campo,callbackchange}){
-    const {data:pacientes}=useCollection("pacientes",{where:["idUsuario","==",fuego.auth().currentUser.uid]})
+    const mod=getModUsuario("pacientes")
+    const {data:pacientes}=useCollection("pacientes",{listen:true,where:["idUsuario","==",fuego.auth().currentUser.uid]})
+    const [openAdd,setOpenAdd]=useState(false)
+    const add=()=>{
+        setOpenAdd(true)
+    }
+    const onLoadPaciente=()=>{
+        setOpenAdd(false)
+    }
     if(!pacientes) return ""
     return(
-        <Select2 callbackchange={callbackchange} campo={campo?campo:"paciente"} 
+        <Grid container>
+            <Grid item xs={10}><Select2 callbackchange={callbackchange} campo={campo?campo:"paciente"} 
         label="Paciente"
          lista={pacientes} campoId="id" 
-            campoLabel={(item)=>`${item.nombre} ${item.apellido} ${item.dni}`}  />
+            campoLabel={(item)=>`${item.nombre} ${item.apellido} ${item.dni}`}  /></Grid>
+           <Grid item xs={1}> <Button onClick={add}><Tooltip title="Agregar Paciente"><Icon className="fas fa-plus"/></Tooltip></Button></Grid>
+        <DialogContenido maxWidth="lg" titulo="AGREGAR PACIENTE" open={openAdd} setOpen={setOpenAdd}>
+            <NuevoPaciente mod={mod} callbackSuccess={onLoadPaciente}/>
+        </DialogContenido>
+        </Grid>
     )
 }
