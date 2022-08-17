6be89e6d5e803f816aa89251ffe5863c5511bb52
alejandro <alejandro@softer.com.ar>
2022-08-17 13:33:35 -0300

    menus y otras

--- components/socios/movimientosCuenta/_formMovimientoCuenta.js
+++ components/socios/movimientosCuenta/_formMovimientoCuenta.js
@@ -1,13 +1,40 @@
-import { Grid } from "@mui/material";
+import { Grid, IconButton, Typography } from "@mui/material";
 import Input from "@components/forms/input";
 import Switch from "@components/forms/switch";
 import SelectProducto from "@components/productos/selectProducto";
 import SelectFecha from "@components/forms/selectorFecha";
 import SelectEstaticFormik from "@components/forms/selectEstaticFormik";
+import SelectFormikAlgolia from "@components/forms/selectAlgoliaFormik";
+import EditarDialogCbu from "@components/cuentasCbu/editarDialog";
+import NuevoDialogCbu from "@components/cuentasCbu/nuevoDialog";
+import { useState } from "react";
 
 export default function Form({ setFieldValue, values }) {
+  const [openEditarCbu, setOpenEditarCbu] = useState(false);
+  const [openNuevoCbu, setOpenNuevoCbu] = useState(false);
   const cambiaProducto = (producto, item) => {
     if (item) setFieldValue("importe", item.importe);
+  };
+  const editoCbuSuccess = (item) => {
+    setOpenEditarCbu(false);
+    setItemsCuenta(item);
+  };
+  const clickEditarCbu = () => {
+    setOpenNuevoCbu(true);
+  };
+  const clickNuevoCbu = () => {
+    setOpenEditarCbu(true);
+  };
+  const setItemsCuenta = (item) => {
+    setFieldValue(`banco`, item.banco);
+    setFieldValue(`cbu`, item.cbu);
+    setFieldValue(`titular`, item.titular);
+    setFieldValue(`nroCuenta`, item.nroCuenta);
+    setFieldValue(`tipoCuenta`, item.tipoCuenta);
+    setFieldValue(`label_tipoCuenta`, item.label_tipoCuenta);
+  };
+  const cambiaCuenta = (item) => {
+    if (item) setItemsCuenta(item);
   };
   return (
     <Grid sx={{ p: 2 }} container spacing={2}>
@@ -39,9 +66,50 @@ export default function Form({ setFieldValue, values }) {
       <Grid item md={3}>
         <Switch label="Debito Automatico " campo="esPorDebitoAutomatico" />
       </Grid>
+      {values.esPorDebitoAutomatico && (
+        <Grid item md={8}>
+          <SelectFormikAlgolia
+            coleccionAlgolia={"cuentasCbu"}
+            label="Cuenta CBU"
+            labelItems={(opt) =>
+              `${opt.titular} ${opt.dniTitular ? opt.dniTitular : "(sin dni)"}`
+            }
+            campo="idCuentaCbu"
+            callbackchange={cambiaCuenta}
+          />
+          <Typography variant="caption">
+            {`CBU: ${values.cbu} BANCO: ${
+              values.tipoCuenta ? values.label_tipoCuenta : "-"
+            }`}{" "}
+            <IconButton
+              size="small"
+              onClick={clickEditarCbu}
+              className="fas fa-pencil"
+              title="Editar"
+            ></IconButton>
+            <IconButton
+              size="small"
+              onClick={clickNuevoCbu}
+              className="fas fa-plus"
+              title="Agregar Cuenta"
+            ></IconButton>
+          </Typography>
+        </Grid>
+      )}
       <Grid item md={12}>
         <Input label="Detalle" campo="detalle" />
       </Grid>
+      <EditarDialogCbu
+        idItem={values.idCuentaCbu}
+        open={openEditarCbu}
+        setOpen={setOpenEditarCbu}
+        callbackSuccess={editoCbuSuccess}
+      />
+      <NuevoDialogCbu
+        open={openEditarCbu}
+        setOpen={setOpenEditarCbu}
+        callbackSuccess={editoCbuSuccess}
+      />
     </Grid>
   );
 }
