1e7d52bb758d4f9d1e9352c3fcb2a55e655d36d8
alejandro <alejandro@softer.com.ar>
2023-07-13 15:22:23 -0300

    tipocuenta

--- components/cuentasCbu/selectTipoCuenta.js
+++ components/cuentasCbu/selectTipoCuenta.js
@@ -2,7 +2,7 @@ import { useCollection, fuego } from "@nandorojo/swr-firestore";
 import Select from "@components/forms/select2Formik";
 import { getModUsuario } from "../../helpers/db";
 export default function Modulo({ label, campo, callbackchange }) {
-  const mod = getModUsuario("cuentasCbu", localStorage.getItem("usermod"));
+  const mod = getModUsuario("cuentasCbu");
   const data = mod.config?.itemsCategoriaSocios;
   if (!data) return "";
   return (
