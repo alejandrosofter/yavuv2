285c975d836622f6f1ec025c72bfd167a61d413a
alejandro <alejandro@softer.com.ar>
2024-09-12 07:02:40 -0300

    clave google

--- config/_firestoreConfig.js
+++ config/_firestoreConfig.js
@@ -1,14 +1,14 @@
-export default function FirestoreConfig(){
-    return {
-        apiKey: "AIzaSyAS_0bOyLhlv9PtmQXiG0Xz5XsMyWYaOqE",
-        authDomain: "yavu-98cac.firebaseapp.com",
-        databaseURL: "https://yavu-98cac-default-rtdb.firebaseio.com",
-        projectId: "yavu-98cac",
-        storageBucket: "yavu-98cac.appspot.com",
-        messagingSenderId: "67091902500",
-        appId: "1:67091902500:web:a611f03c26cd59bc2ee8b1",
-        measurementId: "G-8780TM8YPC"
-      }
+export default function FirestoreConfig() {
+  return {
+    apiKey: "AIzaSyB62mkucXq-58k_THxaSOAfM3YJ3QnVtFc",
+    authDomain: "yavu-98cac.firebaseapp.com",
+    databaseURL: "https://yavu-98cac-default-rtdb.firebaseio.com",
+    projectId: "yavu-98cac",
+    storageBucket: "yavu-98cac.appspot.com",
+    messagingSenderId: "67091902500",
+    appId: "1:67091902500:web:a611f03c26cd59bc2ee8b1",
+    measurementId: "G-8780TM8YPC",
+  };
 }
 
 // {
